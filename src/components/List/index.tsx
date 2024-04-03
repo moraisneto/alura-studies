20eba062c26d4fb5ca48c8e013734abbb934d156
José Morais <josemoraisneto@gmail.com>
2024-04-03 14:05:02 -0400

    Adicionando componente de lista

---
+++ src/components/List/index.tsx
@@ -0,0 +1,19 @@
+const Listagem = () => {
+    return (
+        <aside>
+            <h2>Estudos do dia</h2>
+            <ul>
+                <li>
+                    <h3></h3>
+                    <span></span>
+                </li>
+                <li>
+                    <h3></h3>
+                    <span></span>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+
+export default Listagem
